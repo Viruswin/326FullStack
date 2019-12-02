056066d2db20ffe55ab5fdbeaff72539fcc6e65c
F-0-X <frank.xingyu@gmail.com>
2019-12-02 19:17:28 +0100

    connect success

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ var mongoose = require('mongoose');
     Setup mongo connection
 */
 
-// set mongoURI
-//var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/grocerylist';
 
-var mongoURI = "mongodb+srv://yu:yu@cluster0-y57ho.mongodb.net/test?retryWrites=true&w=majority";
+//const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_URI || 'mongodb+srv://yu:yu@cluster0-y57ho.mongodb.net/test?retryWrites=true&w=majority';
+//const client = new MongoClient(uri, { useNewUrlParser: true });
+//client.connect(err => {
+  //const collection = client.db("test").collection("devices");
+  //// perform actions on the collection object
+  //client.close();
+//});
+
+
+// set mongoURI
+var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/grocerylist';
+
 
 // connect db
-mongoose.connect(mongoURI, {useNewUrlParser: true});
+mongoose.connect(uri, {useNewUrlParser: true});//, {useNewUrlParser: true});
 
 // set port
 var port = process.env.PORT || 1337;
